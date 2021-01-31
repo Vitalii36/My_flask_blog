51d56158e18769e94a04a884dda63a54d6b0aa90
Vitalii36 <vitaliipravdych@gmail.com>
2021-01-31 22:49:34 +0200

    [UPDATE] API

---
+++ app/api/tokens.py
@@ -0,0 +1,35 @@
+from flask import jsonify
+from app import db
+from app.api import bp
+from app.api.auth import basic_auth
+from flask_httpauth import HTTPTokenAuth
+from app.api.auth import token_auth
+
+
+@bp.route('/tokens', methods=['DELETE'])
+@token_auth.login_required
+def revoke_token():
+    token_auth.current_user().revoke_token()
+    db.session.commit()
+    return '', 204
+
+
+@bp.route('/tokens', methods=['POST'])
+@basic_auth.login_required
+def get_token():
+    token = basic_auth.current_user().get_token()
+    db.session.commit()
+    return jsonify({'token': token})
+
+
+token_auth = HTTPTokenAuth()
+
+
+@token_auth.verify_token
+def verify_token(token):
+    return User.check_token(token) if token else None
+
+
+@token_auth.error_handler
+def token_auth_error(status):
+    return error_response(status)
